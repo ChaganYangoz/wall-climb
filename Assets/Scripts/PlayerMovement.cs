2eba7f654febe0e75fcd8b4c8bffd4e0f3ee3a69
ErdemSusam23 <ersusam2003@gmail.com>
2023-08-04 23:39:23 +0300

    add player jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,9 +7,28 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] Rigidbody rb;
 
     [SerializeField] private float speed = 10f;
+    
 
+    private void Update()
+    {
+        
+    }
     private void FixedUpdate()
     {
-        rb.velocity = Vector3.up * speed;
+        //rb.velocity = Vector3.up * speed;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
     }
+
+    private void Jump() 
+    {
+        if (gameObject.transform.position.x < -3)
+        {
+            rb.velocity = Vector3.right * speed;
+        }
+    }
+
 }
